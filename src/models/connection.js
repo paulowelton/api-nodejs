d50b56cab2d6a524bb2e63f7f3199fe7fc39e52b
paulowelton <paulinho.welton08@gmail.com>
2024-05-20 11:13:32 -0300

    retirando informacoes

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise')
 
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'P@ulinho08',
-    database: 'vanturismo'
+    host: '',
+    user: '',
+    password: '',
+    database: ''
 })
 
 module.exports = connection
